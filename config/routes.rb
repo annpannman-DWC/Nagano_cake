4e5507e6919f6b88252eb534b1ab1e9508a58eea
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-17 03:20:42 +0000

    add admin routes view contoroller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,20 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
     sessions: "admin/sessions"
   }
+  namespace :admin do
+    get "/"=>"homes#top"
+    resources :items ,only: [:index,:new,:create,:show,:edit,:update]
+    resources :genres ,only: [:index,:create,:edit,:update]
+    resources :customers ,only: [:index,:show,:edit,:update]
+    resources :orders ,only: [:show,:update]
+    resources :order_details ,only: [:update]
+  end
+
   devise_for :customers, controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   root to: "homes#top"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
