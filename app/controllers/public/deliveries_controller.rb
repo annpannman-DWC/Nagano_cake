33de698905383e75665819e5149a77e5e7deb246
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-18 10:01:02 +0000

    Add deliveries/index

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,2 +1,23 @@
 class Public::DeliveriesController < ApplicationController
+
+  def index
+    @deliveries = Delivery.all
+    @delivery = Delivery.new
+  end
+
+  def edit
+  end
+
+  def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.save
+    redirect_to request.referer
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
