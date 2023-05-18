34a93a2475ebfd64799bc78af5ed59830de6f38f
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-05-18 10:59:35 +0000

    PublicItemsShow AddSeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seeds.rb
+
+# ジャンルの初期データを追加
+Genre.create(name: 'ケーキ')
+Genre.create(name: 'マフィン')
+Genre.create(name: '焼き菓子')
+Genre.create(name: 'チョコレート')
+Genre.create(name: 'プリン')
+
+# ケーキの初期データを追加
+10.times do
+  genre = Genre.all.sample
+
+  Item.create(
+    name: Faker::Dessert.variety,
+    caption: Faker::Lorem.paragraph,
+    price: Faker::Number.within(range: 1000..5000),
+    is_active: Faker::Boolean.boolean(true_ratio: 0.8),
+    genre_id: genre.id
+  )
+end
+
+puts 'Seeding completed!'
