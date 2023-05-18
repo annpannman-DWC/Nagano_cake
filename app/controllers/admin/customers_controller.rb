ba2280c749a4fcbe87363d0b49bdd283ce40b980
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-18 08:52:08 +0000

    add index,edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,17 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
-  end 
-  
-  def show
+    @customers = Customer.all
   end
-  
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
   def edit
-  end 
-  
+  end
+
   def update
-  end 
-  
+  end
+
 end
