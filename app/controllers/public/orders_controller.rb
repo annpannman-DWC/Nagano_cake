ab3da14c19d73a0a6ae2834bd075540c7671c222
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-05-19 07:44:46 +0000

    OrdersEn

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,74 @@
 class Public::OrdersController < ApplicationController
+  
+  def index
+   @orders = current_customer.orders
+  end
+  
+  def new
+    @order = Order.new
+    @addresses = current_customer.addresses
+    @payment_methods = {
+      credit_card: I18n.t("order.payment_methods.credit_card"),
+      transfer: I18n.t("order.payment_methods.transfer"),
+      cash_on_delivery: I18n.t("order.payment_methods.cash_on_delivery")
+    }
+  end
+  
+  def confirm
+    @order = Order.new(order_params)
+    @order.payment_method = params[:order][:payment_method].to_i
+    @order.shipping_cost = 800
+    @cart_items = current_customer.cart_items
+  
+    # 配送先の条件分岐
+    if params[:order][:address_option] == "0"   # 自分の住所
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.address_name = current_customer.full_name
+    elsif params[:order][:address_option] == "1"  # 登録している配送先
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.address_name = @address.address_name
+    elsif params[:order][:address_option] == "2"  # 新たに配送先を指定
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.address_name = params[:order][:address_name]
+    end
+  end
+  
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.shipping_cost = 800
+    @order.save
+  
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      @order_item = OrderItem.new
+      @order_item.order_id = @order.id
+      @order_item.item_id = cart_item.item_id
+      @order_item.quantity = cart_item.quantity
+      @order_item.purchase_price = cart_item.item.price_without_tax * cart_item.quantity * 1.1
+      @order_item.save
+    end
+  
+    current_customer.cart_items.destroy_all
+  
+    redirect_to thanx_path
+  end
+  
+  def show
+    @order = Order.find(params[:id])
+  end
+  
+  def thanx
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :address_name, :payment_method, :total_price, :order_received_status)
+  end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,15 @@
 class Order < ApplicationRecord
+  
+  belongs_to :customer
+  has_many :order_items, dependent: :destroy
 
+  # 注文ステータス
+  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
+
+  # 支払い方法
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  validates :address, presence: true
+  validates :postal_code, presence: true, length: { minimum: 0, maximum: 7 }
+  validates :address_name, presence: true
 end
