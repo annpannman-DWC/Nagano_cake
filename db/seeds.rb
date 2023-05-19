7fc11c35b3479b031882619c55567afc3a254cfe
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-05-19 04:12:02 +0000

    SeedsClean

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,42 @@
 
 # seeds.rb
 
-# ジャンルの初期データを追加
-Genre.create(name: 'ケーキ')
-Genre.create(name: 'マフィン')
-Genre.create(name: '焼き菓子')
-Genre.create(name: 'チョコレート')
-Genre.create(name: 'プリン')
+# # ジャンルの初期データを追加
+# Genre.create(name: 'ケーキ')
+# Genre.create(name: 'マフィン')
+# Genre.create(name: '焼き菓子')
+# Genre.create(name: 'チョコレート')
+# Genre.create(name: 'プリン')
 
-# ケーキの初期データを追加
-10.times do
-  genre = Genre.all.sample
+# # ケーキの初期データを追加
+# genre1 = Genre.find_by(name: "ケーキ")
+# genre2 = Genre.find_by(name: "パフェ")
+# genre3 = Genre.find_by(name: "クッキー")
 
-  Item.create(
-    name: Faker::Dessert.variety,
-    caption: Faker::Lorem.paragraph,
-    price: Faker::Number.within(range: 1000..5000),
-    is_active: Faker::Boolean.boolean(true_ratio: 0.8),
-    genre_id: genre.id
-  )
-end
+# unless genre1.nil? || genre2.nil? || genre3.nil?
+#   Item.create(
+#     name: "チョコレートケーキ",
+#     caption: "濃厚なチョコレートのケーキです。",
+#     price: 2500,
+#     is_active: true,
+#     genre_id: genre1.id
+#   )
 
-puts 'Seeding completed!'
+#   Item.create(
+#     name: "ストロベリーパフェ",
+#     caption: "新鮮なイチゴとヨーグルトのパフェです。",
+#     price: 1800,
+#     is_active: true,
+#     genre_id: genre2.id
+#   )
+
+#   Item.create(
+#     name: "バターココアクッキー",
+#     caption: "バターとココアの風味が楽しめるクッキーです。",
+#     price: 1200,
+#     is_active: true,
+#     genre_id: genre3.id
+#   )
+
+#   puts 'Seeding completed!'
+# end
