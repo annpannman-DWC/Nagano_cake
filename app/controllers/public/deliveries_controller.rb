2f3e734f88d318c5edbfd61e62b039438d74afd6
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-19 03:56:05 +0000

    Add delivery table

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -10,14 +10,26 @@ class Public::DeliveriesController < ApplicationController
 
   def create
     @delivery = Delivery.new(delivery_params)
-    @delivery.save
-    redirect_to request.referer
+    # @delivery.customer_id = current_customer.id
+    if @delivery.save!
+      redirect_to request.referer
+    else
+      @deliveries = Delivery.all
+      render :index
+    end
   end
 
   def update
   end
 
   def destroy
+    @delivery = Delivery.find(params[:id])
+    @delivery.destroy
+    redirect_to request.referer
   end
 
+  private
+  def delivery_params
+    params.require(:delivery).permit(:postal_code, :address, :address_name)
+  end
 end
