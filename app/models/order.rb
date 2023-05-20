fa8d852cc6e29d24a8131346a38a41c067b1be59
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-20 07:47:42 +0000

    Add enum order_received_status to order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,14 +3,12 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
 
-  # 注文ステータス
-  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
+  # 受注ステータス
+  enum order_received_status: { wait: 0, check: 1, making: 2, standby: 3, sent: 4 }
 
   # 支払い方法
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
-  #お届け先住所 
-  #enum address: { my_address: 0, registration_address: 1, new_address: 2 }
+
 
   validates :address, presence: true
   validates :postal_code, presence: true, length: { minimum: 0, maximum: 7 }
