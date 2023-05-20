710ae5fc3a77206419fc47f3e688f082d142e197
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-05-20 02:55:43 +0000

    Add CustomerEditPage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,13 +1,33 @@
 class Public::CustomersController < ApplicationController
+  before_action :set_customer, only: %i[edit show]
+  
   def edit
+    # @customer = Customer.new
   end
+  
   def show
-    @customer = current_customer
   end
+  
   def update
+        @customer = current_customer
+    @customer.update(customer_params)
+redirect_to customers_mypage_path
   end
+  
   def confirm
   end
+  
   def withdraw
   end
+  
+  private
+  
+  def set_customer
+    @customer = current_customer
+  end
+  
+  def customer_params
+    params.require(:customer).permit(:email, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone_number)
+  end
+  
 end
