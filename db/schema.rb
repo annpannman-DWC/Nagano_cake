2b5ec758949281ecd9e9285a28466927be099005
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-21 02:52:59 +0000

    migration shopping

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_19_070524) do
+ActiveRecord::Schema.define(version: 2023_05_18_040232) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,13 +38,6 @@ ActiveRecord::Schema.define(version: 2023_05_19_070524) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "addresses", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "customer_id", null: false
-    t.index ["customer_id"], name: "index_addresses_on_customer_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -124,11 +117,11 @@ ActiveRecord::Schema.define(version: 2023_05_19_070524) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.integer "shipping_cost", null: false
+    t.integer "shopping_cost", null: false
     t.string "address_name", default: "", null: false
     t.text "address", default: "", null: false
     t.string "postal_code", default: "", null: false
-    t.integer "payment_method", null: false
+    t.integer "payment_method", default: 0, null: false
     t.integer "total_price", null: false
     t.integer "order_received_status", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -137,5 +130,4 @@ ActiveRecord::Schema.define(version: 2023_05_19_070524) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addresses", "customers"
 end
