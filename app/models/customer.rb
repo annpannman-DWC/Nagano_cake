0ea5b471025b29fc133b7b7753d4286025bedb99
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-21 10:00:31 +0000

    Add order_customer to homes/top

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,13 +1,13 @@
 class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :deliveries, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
-  
+
   def full_name
-    "#{last_name} #{first_name}"
+    "#{first_name} #{last_name}"
   end
 
 end
