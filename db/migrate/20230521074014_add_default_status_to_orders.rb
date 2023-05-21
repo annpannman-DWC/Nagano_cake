d6316bd58eefeb386705ae7f125294af08290386
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-21 09:31:12 +0000

    Remove _root to _header.html

--- db/migrate/20230521074014_add_default_status_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultStatusToOrders < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :orders, :order_received_status, 0
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_21_074014) do
+ActiveRecord::Schema.define(version: 2023_05_18_040232) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
