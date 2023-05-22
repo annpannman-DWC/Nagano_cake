405c887c8031d4e060678b15072972e2584238de
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-22 07:19:19 +0000

    change_model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
-  
+
   has_one_attached :image
-  
+
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
-  
+
   validates :name, presence: true
   validates :caption, length: { minimum: 0, maximum: 500 }
   validates :price, presence: true
-  
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -17,17 +17,17 @@ class Item < ApplicationRecord
     end
       image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def self.search_for(search,word)
     if search == 'perfect'
       Item.where(name: word)
     elsif search == 'forward'
-      Item.where('name LIkE ?', word + '%')
+      Item.where('name LIkE ?', '#{word}%')
     elsif search == 'backward'
-      Item.where('name LIkE ?', '%' + word)
+      Item.where('name LIkE ?', '%#{word}')
     else
-      Item.where('name LIKE ?', '%' + word + '%')
+      Item.where('name LIKE ?', '%#{word}%')
     end
   end
-  
+
 end
