3c473f33de6d03d123942f9a1cb5c47b3ab2b0a9
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-22 05:52:27 +0000

    search_bug

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,17 @@
 class Genre < ApplicationRecord
-  
+
   has_many :items
-  
+
+  def self.search_for(search,word)
+    if search == 'perfect'
+      Genre.where(name: word)
+    elsif search == 'forward'
+      Genre.where('name LIkE ?', word + '%')
+    elsif search == 'backward'
+      Genre.where('name LIkE ?', '%' + word)
+    else
+      Genre.where('name LIKE ?', '%' + word + '%')
+    end
+  end
+
 end
