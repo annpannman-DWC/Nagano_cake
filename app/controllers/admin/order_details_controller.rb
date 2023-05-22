d0ce4c495131d01a774f1a4322c71896073c9fa7
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-22 10:15:01 +0000

    Add making_status(getting an error)

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,16 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
+    @order = Order.find(params[:id])
+    @order_detail = OrderDetail.new
+    @order_detail.update(order_detail_params)
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:makeing_status)
   end
 
 end
