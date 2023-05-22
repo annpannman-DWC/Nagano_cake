136cd0bd31437f47af6011d5f9a30a44649697dc
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-22 04:29:08 +0000

    Add order_received_status to admin/orders/show

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,6 +3,13 @@ class Admin::HomesController < ApplicationController
 
   def top
     @orders = Order.all
+    @order = Order.new
   end
+
+  private
+
+  # def order_params
+  #   params.require(:order).permit(:order_received_status)
+  # end
 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,15 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_received_status)
   end
 
 end
