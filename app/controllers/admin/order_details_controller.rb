cef2cfd2119d81256545c0889f188a666a010265
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-23 05:56:49 +0000

    Add making_status to admin/orders/show

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,8 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    @order = Order.find(params[:id])
-    @order_detail = OrderDetail.new
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
     @order_detail.update(order_detail_params)
     redirect_to admin_order_path(@order)
   end
